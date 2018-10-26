feda3fa7c1b3e3eeb6472871144462b1c51e1609
Anton Hirov <hirov.anton@yahoo.com>
2018-10-27 02:42:59 +0300

    Added simple ssl certificate analyzer

---
+++ CertificateAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace X509
+{
+    internal sealed class CertificateAnalyzer
+    {
+        public void Analyze()
+        {
+            Console.WriteLine("SSL Certificate analyzer...");
+            Console.WriteLine("Enter url address (eg, https://resolver.technology):");
+
+            var address = Console.ReadLine();
+            if (string.IsNullOrEmpty(address))
+                throw new InvalidOperationException("No input...");
+
+            var uri = new Uri(address);
+            var port = uri.Port;
+            if (port != 443)
+                throw new InvalidOperationException("Not https://");
+
+            ServicePointManager.ServerCertificateValidationCallback = Analyze;
+            var request = WebRequest.Create(uri);
+            using (request.GetResponse()) { }
+        }
+
+        private static bool Analyze(object sender,
+                                    X509Certificate certificate,
+                                    X509Chain chain,
+                                    SslPolicyErrors sslPolicyErrors)
+        {
+            Console.WriteLine(certificate.ToString(true));
+            Console.WriteLine($"Errors: {sslPolicyErrors}");
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace X509
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private const int SwpShowWindow = 0x0040;
+        private static IntPtr Handle => GetConsoleWindow();
+
+        [DllImport("kernel32")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32")]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter,
+                                                int x, int y, int cx, int cy, int uFlags);
+
+        private static void Main(string[] args)
         {
+            try
+            {
+                SetWindowPosition();
+                new CertificateAnalyzer().Analyze();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.WriteLine("########## Press Enter to close the console ##########");
+            Console.ReadLine();
+        }
+
+        private static void SetWindowPosition()
+        {
+            var screen = Screen.PrimaryScreen.Bounds;
+            SetWindowPos(Handle, IntPtr.Zero,
+                         screen.Width*2/3, 0, screen.Width/3, screen.Height,
+                         SwpShowWindow);
         }
     }
 }
